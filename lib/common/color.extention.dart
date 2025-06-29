18b6727b161789c4f27cf173eeb509f6e7cf4b4c
ASUS <shehansandakalum2003@gmail.com>
2025-06-29 20:00:20 +0530

    Update some codes

--- lib/common/color.extention.dart
+++ lib/common/color.extention.dart
@@ -3,4 +3,20 @@ import 'package:flutter/material.dart';
 class Tcolor {
   static Color get primary => const Color(0xff5ABDBC);
   static Color get primartLight => const Color(0xffA4D8E1);
+  static Color get text => const Color.fromARGB(255, 26, 26, 26);
+  static Color get subTitle => const Color.fromARGB(255, 100, 100, 100).withOpacity(0.4);
+
+
+  static List<Color> get button => [
+        const Color(0xff5ABDBC),
+        const Color(0xffA4D8E1),
+      ];
+
+  static List<Color> get list => [
+        const Color(0xff5ABDBC),
+        const Color(0xffA4D8E1),
+        const Color(0xffF2F2F2),
+        const Color(0xffF2F2F2),
+        const Color(0xffF2F2F2),
+  ];
 }
