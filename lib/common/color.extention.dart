b1c97fade76d6b9325c76f699398343628a13b15
ASUS <shehansandakalum2003@gmail.com>
2025-07-22 15:37:35 +0530

    edit color edtention

--- lib/common/color.extention.dart
+++ lib/common/color.extention.dart
@@ -4,19 +4,22 @@ class Tcolor {
   static Color get primary => const Color.fromARGB(255, 2, 116, 118);
   static Color get primartLight => const Color.fromARGB(255, 14, 191, 222);
   static Color get text => const Color.fromARGB(255, 26, 26, 26);
-  static Color get subTitle => const Color.fromARGB(255, 100, 100, 100).withOpacity(0.4);
+  static Color get subTitle =>
+      const Color.fromARGB(255, 95, 94, 94).withOpacity(0.4);
 
+  static Color get textBox =>
+      const Color.fromARGB(255, 151, 151, 151).withOpacity(0.6);
 
   static List<Color> get button => [
-        const Color(0xff5ABDBC),
-        const Color(0xffA4D8E1),
-      ];
+    const Color(0xff5ABDBC),
+    const Color(0xffA4D8E1),
+  ];
 
   static List<Color> get list => [
-        const Color(0xff5ABDBC),
-        const Color(0xffA4D8E1),
-        const Color(0xffF2F2F2),
-        const Color(0xffF2F2F2),
-        const Color(0xffF2F2F2),
+    const Color(0xff5ABDBC),
+    const Color(0xffA4D8E1),
+    const Color(0xffF2F2F2),
+    const Color(0xffF2F2F2),
+    const Color(0xffF2F2F2),
   ];
 }
